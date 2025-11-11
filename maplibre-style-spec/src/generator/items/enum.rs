7317b1dcbf8f7625ed5efeb8a146f92e11eaffdc
Frank Elsinga <frank@elsinga.de>
2025-11-11 10:25:47 +0100

    simplify enum handling

--- maplibre-style-spec/src/generator/items/enum.rs
+++ maplibre-style-spec/src/generator/items/enum.rs
@@ -1,7 +1,8 @@
+use std::collections::BTreeMap;
 use codegen::Scope;
-use serde_json::Value;
+use serde_json::{Number, Value};
 
-use crate::decoder::{EnumValues, Fields};
+use crate::decoder::{EnumDocs, EnumValues, Fields};
 use crate::generator::autotest::generate_test_from_example_if_present;
 use crate::generator::formatter::to_upper_camel_case;
 
@@ -14,37 +15,10 @@ pub fn generate(
 ) {
     match values {
         EnumValues::Version(values) => {
-            assert!(values.len() <= u8::MAX as usize);
-
-            let enu = scope
-                .new_enum(name)
-                .doc(&common.doc)
-                .vis("pub")
-                .repr("u8")
-                .derive("serde::Deserialize, PartialEq, Eq, Debug, Clone, Copy");
-            for v in values {
-                enu.new_variant(to_upper_camel_case(&v.to_string()))
-                    .discriminant(v.to_string());
-            }
-            assert!(
-                values
-                    .iter()
-                    .all(|v| v.as_u64().is_some_and(|v| v <= u8::MAX as u64))
-            );
+            generate_version(scope, name, &common, values);
         }
         EnumValues::Enum(values) => {
-            let enu = scope
-                .new_enum(name)
-                .doc(&common.doc)
-                .vis("pub")
-                .derive("serde::Deserialize, PartialEq, Eq, Debug, Clone, Copy");
-            for (key, value) in values {
-                let var_name = to_upper_camel_case(key);
-                let var = enu.new_variant(&var_name).doc(&value.doc);
-                if key != &var_name {
-                    var.annotation(format!("#[serde(rename=\"{key}\")]"));
-                }
-            }
+            generate_regular_enum(scope, name, &common, values);
         }
     }
 
@@ -60,4 +34,40 @@ pub fn generate(
             ));
     }
     generate_test_from_example_if_present(scope, name, common);
+}
+
+
+fn generate_regular_enum(scope: &mut Scope, name: &str, common: &&Fields, values: &BTreeMap<String, EnumDocs>) {
+    let enu = scope
+        .new_enum(name)
+        .doc(&common.doc)
+        .vis("pub")
+        .derive("serde::Deserialize, PartialEq, Eq, Debug, Clone, Copy");
+    for (key, value) in values {
+        let var_name = to_upper_camel_case(key);
+        let var = enu.new_variant(&var_name).doc(&value.doc);
+        if key != &var_name {
+            var.annotation(format!("#[serde(rename=\"{key}\")]"));
+        }
+    }
+}
+
+fn generate_version(scope: &mut Scope, name: &str, common: &&Fields, values: &Vec<Number>) {
+    assert!(values.len() <= u8::MAX as usize);
+
+    let enu = scope
+        .new_enum(name)
+        .doc(&common.doc)
+        .vis("pub")
+        .repr("u8")
+        .derive("serde::Deserialize, PartialEq, Eq, Debug, Clone, Copy");
+    for v in values {
+        enu.new_variant(to_upper_camel_case(&v.to_string()))
+            .discriminant(v.to_string());
+    }
+    assert!(
+        values
+            .iter()
+            .all(|v| v.as_u64().is_some_and(|v| v <= u8::MAX as u64))
+    );
 }
